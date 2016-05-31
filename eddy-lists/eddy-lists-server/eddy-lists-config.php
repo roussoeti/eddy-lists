f86436c17635e79db42a0b8aef9286f74ebdc5bf
EddySoft <rousso.eti@gmail.com>
2016-05-31 18:31:04 -0400

    MAJ avec JSON

--- eddy-lists/eddy-lists-server/eddy-lists-config.php
+++ eddy-lists/eddy-lists-server/eddy-lists-config.php
@@ -1,5 +1,17 @@
 <?php
 
+
 $link = mysqli_connect("localhost", "eddySoft", "eddy$1511", "eddylists");	 
+
+    function utf8ize($d) {
+        if (is_array($d)) {
+            foreach ($d as $k => $v) {
+                $d[$k] = utf8ize($v);
+            }
+        } else if (is_string ($d)) {
+            return utf8_encode($d);
+        }
+        return $d;
+    }
 
 ?>
